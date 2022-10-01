8994903b85de39c5a332766fac221ff764adf975
Huy Phan <phan.q.huy21@gmail.com>
2022-10-01 17:31:06 -0400

    finihed hw 2

--- ps2/PS2.P2.js
+++ ps2/PS2.P2.js
@@ -1,15 +1,12 @@
 // Write a generator that is initialized with a sentence and that emits each word of the sentence in turn
 
-function* sentenceGenerator(sentence) {
-    let words = sentence.split(" ");
-    for (const word of words) {
-        yield word;
-    }
+function* everyWord(sentence) {
+    let sentArr = sentence.split(" ");
+    yield* sentArr;
+
 }
 
 let sent = "All I know is something like a bird within her sang"
-let i = sent.split(" ").length
-
-while (i -- > 0){
-    console.log(sentenceGenerator(sent).next().value)
+for (const word of everyWord(sent)) {
+    console.log(word)
 }
